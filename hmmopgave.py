2845974a548844ed8959b75bb8d9d10f8dcf85e9
DLCHunter <simonlamberth@hotmail.com>
2026-02-28 14:57:45 +0100

    First part done

--- hmmopgave.py
+++ hmmopgave.py
@@ -18,9 +18,11 @@ T = 100
 
 C1 = 2
 Clist = [C1]
+Zlist = []
 
 random.seed(0)
 
+# Probability of moving forward
 def moveForward(state):
     if state == 0:
         return 0 if random.random() < state0probs[0] else 2
@@ -29,10 +31,37 @@ def moveForward(state):
     else:
         return 0 if random.random() < state2probs[0] else (1 if random.random() < state2probs[1] else 2)
 
+
+
+# Simulate the process T times, starting (always) from state 2, and save the resulting states after n steps
 startstate = 2
 for i in range(T):
     state = startstate
+    # Move n steps forward and save state
     for t in range(n):
         state = moveForward(state)
+    # Add the state to the list
     Clist.append(state)
-print(Clist)
+
+for c in Clist:
+    match c:
+        case 0:
+            if random.random() < 1-alpha:
+                Zlist.append(1)
+            else:
+                Zlist.append(0)
+        case 1:
+            if random.random() < alpha:
+                Zlist.append(1)
+            else:
+                Zlist.append(0)
+        case 2:
+            if random.random() < 0.5:
+                Zlist.append(1)
+            else:
+                Zlist.append(0)
+
+lambdalist = [l0 if z == 0 else l1 for z in Zlist]
+meanlambdalist = np.mean(lambdalist)
+Xlist = [np.random.poisson(lambd) for lambd in lambdalist]
+print("Poisson samples: ", Xlist)
